37a6ed5679af2048643be0daf40813ed088ad9a3
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-02-15 18:25:14 +0200

    footer updated

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">103 Club avenue, Waterkloof Heights, Pretoria.</p>
         <p className="p__opensans">+1 212-344-1230</p>
         <p className="p__opensans">+1 212-555-1230</p>
       </div>
@@ -39,7 +39,7 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">&copy; {new Date().getFullYear()} Gericht. All Rights reserved.</p>
     </div>
 
   </div>
