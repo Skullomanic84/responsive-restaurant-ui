8bd989d629aa68dcba0430422af8a253e5985dc8
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-02-15 09:19:27 +0200

    Update AboutUs.jsx
    completed About section, added media query to Header section and updated global font from open san to quicksand.

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
-
+//styles
 import './AboutUs.css';
+//components
+import { images } from '../../constants';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
+  
+    <div className="app__aboutus app__bg flex__center section__padding" id="#about">
+      {/* center piece */}
+      <div className="app__aboutus-overlay flex__center">
+        <img src={images.G} alt="letter g" />
+      </div>
+      {/* About us main section */}
+      <div className="app__aboutus-content flex__center">
+
+        {/* about us section */}
+        <div className="app__aboutus-content_about">
+          <h1 className="headtext__cormorant">About us</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+          <p className="p__opensans">Gerich is a family owned restaurant run by Alex and Tina. Situated in the heart of Fourways with quintessential views overlooking beautiful old buildings and oak trees, Gerich’s serves simple, contemporary food made using locally sourced, seasonal ingredients.</p>
+
+          <button type='button' className="custom__button">Know more</button>
+        </div>
+
+        {/* knife section */}
+          <div className="app__aboutus-content_knife flex__center">
+            <img src={images.knife} alt="about_knife"  />
+          </div>
+
+        {/* history section */}
+        <div className="app__aboutus-content_history">
+          <h1 className="headtext__cormorant">Our History</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+          <p className="p__opensans">Gerich is a family owned restaurant run by Alex and Tina. Situated in the heart of Fourways with quintessential views overlooking beautiful old buildings and oak trees, Gerich’s serves simple, contemporary food made using locally sourced, seasonal ingredients.</p>
+
+          <button type='button' className="custom__button">Know more</button>
+        </div>
+      </div>
+    </div>
+  
 );
 
 export default AboutUs;
